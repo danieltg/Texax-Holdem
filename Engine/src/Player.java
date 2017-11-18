3174de770be7765fad5b69be34f33ee74b7c7e19
Dfilian <33777081+Dfilian@users.noreply.github.com>
2017-11-18 13:56:33 +0200

    Player members added

--- Engine/src/Player.java
+++ Engine/src/Player.java
@@ -1,4 +1,12 @@
 public abstract class Player {
+    private char playerType;
+    private char state;
+    private boolean isActive;
+    private int cheeps;
+    private Cards [] playerCards=new Cards[2];
+    private int numOfBuys;
+    private int handsWon;
     
+
 
 }
